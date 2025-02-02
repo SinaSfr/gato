6777c56ffa01e59701030f7d44b4cff0956af4f0
kmg.d102 <kmg.d102@gmail.com>
2025-02-02 06:04:39 -0800

    project started

---
+++ tailwind.config.web.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./web/**/*.html", "./web/assets/js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        secondary: "#A21CAF",
+      },
+    },
+  },
+  plugins: [],
+};
